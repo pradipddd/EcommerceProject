f0fb2405b380f58c44ace857082f1d8253d70f37
pradip <you@example.com>
2021-09-30 23:51:29 +0530

    file upload

---
+++ EcommerceProject/Accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import LoginView, LogoutView, RegisterView, SellerLoginView, SellerLogoutView, SellerRegisterView, SellerShowView, ShowView, homeview
+
+
+
+urlpatterns=[
+    path('',homeview,name='home'),
+    path('reg/',RegisterView,name='register'),
+    path('log/',LoginView,name='login'),
+    path('lout/',LogoutView,name='logout'),
+    path('show/',ShowView,name='show'),
+    path('sreg/',SellerRegisterView,name='sellerregister'),
+    path('slog/',SellerLoginView,name='sellerlogin'),
+    path('slout/',SellerLogoutView,name='sellerlogout'),
+    path('sshow/',SellerShowView,name='sellershow'),
+]
